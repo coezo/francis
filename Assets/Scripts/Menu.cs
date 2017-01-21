3d0a2632033d79ef70071a5ed2a8818f8e679c33
Pedro <pedro@Rodrigos-MacBook.local>
2017-01-21 17:07:48 -0300

    Menu, Creditos e Tela de Setup adicionados.

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void StartTutorial() {
+
+		SceneManager.LoadScene ("Setup");
+
+	}
+
+	public void StartCredits() {
+
+		SceneManager.LoadScene ("Credits");
+
+	}
+
+	public void StartHome() {
+
+		SceneManager.LoadScene ("Home");
+
+	}
+
+	public void StartLevel() {
+
+		SceneManager.LoadScene ("Cave");
+
+	}
+
+
+}
+
