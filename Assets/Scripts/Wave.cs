cf91aa6860a84934010229da9b3f82b3a33f11ab
Victor Soares <flicko@gmail.com>
2017-01-21 15:28:05 -0300

    - Mudanca do bounciness da wave para total - Implementacao do tempo de fading da wave (padrao 1 segundo)

--- Assets/Scripts/Wave.cs
+++ Assets/Scripts/Wave.cs
@@ -5,9 +5,25 @@ using UnityEngine;
 public class Wave : MonoBehaviour {
 
 	public float speed;
+	public float fadingTime;
+
+	private Light light;
+
+	void Awake(){
+		light = GetComponent<Light> ();
+	}
 
 	public void Shoot () {
 		this.GetComponent<Rigidbody2D> ().AddForce ( transform.right * speed);
+		InvokeRepeating ("Fade", 0, 0.1f * fadingTime);
+	}
+
+	private void Fade(){
+		light.intensity -= 0.1f;
+		if (light.intensity <= 0) {
+			CancelInvoke ();
+			Destroy (this.gameObject);
+		}
 	}
 
 }
